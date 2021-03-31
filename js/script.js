22e9b1de9be84cea0e9cb69fb81c44389d8fab41
Mariem Ehab <45633841+MaryEhb@users.noreply.github.com>
2021-03-31 13:02:37 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//Variables
+let questions = document.querySelectorAll(".question-btn");
+let questionsBlock = document.querySelectorAll(".question-wrap");
+
+//Function called to display or remove the answer
+let press = function (){
+    let hasclass = false;
+    if(this.parentElement.parentElement.classList.contains('pressed')){
+        hasclass = true;
+    }
+    
+    questions.forEach(question => {
+        question.parentElement.parentElement.classList.remove('pressed');
+    })
+
+    if(!hasclass){
+        this.parentElement.parentElement.classList.add('pressed');
+    }
+}
+
+//Add function to element(when clicked)
+questions.forEach(question => {
+    question.addEventListener('click', press);
+})
